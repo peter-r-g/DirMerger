829352db65fe7de8a4c78a8b9850efa9df4056c3
Peter <petergorman11@gmail.com>
2019-07-09 14:42:12 -0400

    Read in base directories and files from selected dir, need recursion here

--- DirMerger/DirMerger/Form1.cs
+++ DirMerger/DirMerger/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,9 @@ namespace DirMerger
 {
     public partial class Form1 : Form
     {
+        private string resourceDir = Application.StartupPath + "\\resource\\";
+        public string curDir = Environment.CurrentDirectory;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,12 +24,48 @@ namespace DirMerger
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            dirBrowser.ShowDialog();
+            // Load tree view images
+            ImageList treeViewImages = new ImageList();
+            treeViewImages.Images.Add(Image.FromFile(resourceDir + "file-icon.png"));
+            treeViewImages.Images.Add(Image.FromFile(resourceDir + "folder-icon.png"));
+
+            dirTreeView.ImageList = treeViewImages;
+
+            // Get the folder we're working in
+            DialogResult result = dirBrowser.ShowDialog();
+
+            // Make sure we actually got a folder
+            if (result == DialogResult.OK)
+                curDir = dirBrowser.SelectedPath;
+            else
+                throw new Exception("Program did not receive a directory to use!");
+
+            // Get all directories and files in there
+            string[] directories = Directory.GetDirectories(curDir);
+            string[] files = Directory.GetFiles(curDir);
+
+            // Start updating the tree view
+            dirTreeView.BeginUpdate();
+            foreach (string dir in directories)
+            {
+                string formattedDir = dir.Replace(curDir, "");
+                TreeNode dirNode = new TreeNode(formattedDir);
+                dirNode.ImageIndex = 1;
+                dirTreeView.Nodes.Add(dirNode);
+            }
+            foreach (string file in files)
+            {
+                string formattedFile = file.Replace(curDir, "");
+                TreeNode fileNode = new TreeNode(formattedFile);
+                fileNode.ImageIndex = 0;
+                dirTreeView.Nodes.Add(fileNode);
+            }
+            dirTreeView.EndUpdate();
         }
 
         private void DirBrowser_HelpRequest(object sender, EventArgs e)
         {
-
+            
         }
     }
 }
