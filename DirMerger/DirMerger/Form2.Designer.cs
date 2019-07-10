ceb8e7016145b8338491c25cb1c78dc978434c13
Peter <petergorman11@gmail.com>
2019-07-10 12:21:25 -0400

    Add code editing/saving

--- DirMerger/DirMerger/Form2.Designer.cs
+++ DirMerger/DirMerger/Form2.Designer.cs
@@ -35,6 +35,9 @@
             this.textZoomOption2 = new System.Windows.Forms.ToolStripMenuItem();
             this.textZoomOption3 = new System.Windows.Forms.ToolStripMenuItem();
             this.textZoomOption4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.editMenuTab = new System.Windows.Forms.ToolStripMenuItem();
+            this.editingOption = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveOption = new System.Windows.Forms.ToolStripMenuItem();
             this.mainMenu.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -48,10 +51,12 @@
             this.codeViewer.Text = "";
             this.codeViewer.WordWrap = false;
             this.codeViewer.ZoomFactor = 2F;
+            this.codeViewer.TextChanged += new System.EventHandler(this.CodeViewer_TextChanged);
             // 
             // mainMenu
             // 
             this.mainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editMenuTab,
             this.textZoomMenuTab});
             this.mainMenu.Location = new System.Drawing.Point(0, 0);
             this.mainMenu.Name = "mainMenu";
@@ -75,28 +80,51 @@
             this.textZoomOption1.Name = "textZoomOption1";
             this.textZoomOption1.Size = new System.Drawing.Size(180, 22);
             this.textZoomOption1.Text = "0.5x";
-            this.textZoomOption1.Click += new System.EventHandler(this.textZoomOption_Click);
+            this.textZoomOption1.Click += new System.EventHandler(this.TextZoomOption_Click);
             // 
             // textZoomOption2
             // 
             this.textZoomOption2.Name = "textZoomOption2";
             this.textZoomOption2.Size = new System.Drawing.Size(180, 22);
             this.textZoomOption2.Text = "1x";
-            this.textZoomOption2.Click += new System.EventHandler(this.textZoomOption_Click);
+            this.textZoomOption2.Click += new System.EventHandler(this.TextZoomOption_Click);
             // 
             // textZoomOption3
             // 
             this.textZoomOption3.Name = "textZoomOption3";
             this.textZoomOption3.Size = new System.Drawing.Size(180, 22);
             this.textZoomOption3.Text = "1.5x";
-            this.textZoomOption3.Click += new System.EventHandler(this.textZoomOption_Click);
+            this.textZoomOption3.Click += new System.EventHandler(this.TextZoomOption_Click);
             // 
             // textZoomOption4
             // 
             this.textZoomOption4.Name = "textZoomOption4";
             this.textZoomOption4.Size = new System.Drawing.Size(180, 22);
             this.textZoomOption4.Text = "2x";
-            this.textZoomOption4.Click += new System.EventHandler(this.textZoomOption_Click);
+            this.textZoomOption4.Click += new System.EventHandler(this.TextZoomOption_Click);
+            // 
+            // editMenuTab
+            // 
+            this.editMenuTab.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editingOption,
+            this.saveOption});
+            this.editMenuTab.Name = "editMenuTab";
+            this.editMenuTab.Size = new System.Drawing.Size(39, 20);
+            this.editMenuTab.Text = "Edit";
+            // 
+            // editingOption
+            // 
+            this.editingOption.Name = "editingOption";
+            this.editingOption.Size = new System.Drawing.Size(180, 22);
+            this.editingOption.Text = "Start Editing";
+            this.editingOption.Click += new System.EventHandler(this.EditingOption_Click);
+            // 
+            // saveOption
+            // 
+            this.saveOption.Name = "saveOption";
+            this.saveOption.Size = new System.Drawing.Size(180, 22);
+            this.saveOption.Text = "Save";
+            this.saveOption.Click += new System.EventHandler(this.SaveOption_Click);
             // 
             // Form2
             // 
@@ -108,6 +136,7 @@
             this.MainMenuStrip = this.mainMenu;
             this.Name = "Form2";
             this.Text = "Code Viewer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form2_FormClosing);
             this.Load += new System.EventHandler(this.Form2_Load);
             this.Resize += new System.EventHandler(this.Form2_Resize);
             this.mainMenu.ResumeLayout(false);
@@ -126,5 +155,8 @@
         private System.Windows.Forms.ToolStripMenuItem textZoomOption2;
         private System.Windows.Forms.ToolStripMenuItem textZoomOption3;
         private System.Windows.Forms.ToolStripMenuItem textZoomOption4;
+        private System.Windows.Forms.ToolStripMenuItem editMenuTab;
+        private System.Windows.Forms.ToolStripMenuItem editingOption;
+        private System.Windows.Forms.ToolStripMenuItem saveOption;
     }
 }
--- DirMerger/DirMerger/Form2.cs
+++ DirMerger/DirMerger/Form2.cs
@@ -16,28 +16,75 @@ namespace DirMerger
     {
         public string filePath;
 
+        bool editing = false;
+        bool saved = true;
+
         public Form2(string filePath)
         {
             this.filePath = filePath;
             InitializeComponent();
         }
 
+        private void SaveFile()
+        {
+            saved = true;
+            File.WriteAllLines(filePath, codeViewer.Lines);
+        }
+
         private void Form2_Load(object sender, EventArgs e)
         {
-            this.Text = "Code Viewer - " + this.filePath;
+            this.Text = "Code Viewer - " + filePath;
 
             string[] fileLines = File.ReadAllLines(filePath);
             codeViewer.Lines = fileLines;
+            saved = true;
+        }
+
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!saved)
+            {
+                DialogResult result = MessageBox.Show("There are changes to this file that haven't been saved, would you like to save the file?", "Unsaved File", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+                if (result == DialogResult.Yes)
+                    SaveFile();
+            }
         }
 
         private void Form2_Resize(object sender, EventArgs e)
         {
-            Size newSize = Form2.ActiveForm.Size;
+            Size newSize = this.Size;
             codeViewer.Location = new Point(12, 27);
             codeViewer.Size = new Size(newSize.Width - 40, newSize.Height - 78);
         }
 
-        private void textZoomOption_Click(object sender, EventArgs e)
+        private void CodeViewer_TextChanged(object sender, EventArgs e)
+        {
+            saved = false;
+        }
+
+        private void EditingOption_Click(object sender, EventArgs e)
+        {
+            if (editing)
+            {
+                editing = false;
+                editingOption.Text = "Start Editing";
+                codeViewer.ReadOnly = true;
+            }
+            else
+            {
+                editing = true;
+                editingOption.Text = "Stop Editing";
+                codeViewer.ReadOnly = false;
+            }
+        }
+
+        private void SaveOption_Click(object sender, EventArgs e)
+        {
+            SaveFile();
+        }
+
+        private void TextZoomOption_Click(object sender, EventArgs e)
         {
             codeViewer.ZoomFactor = float.Parse(sender.ToString().Replace('x', ' '));
         }
